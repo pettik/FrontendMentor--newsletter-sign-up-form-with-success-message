e6ade48e1df3a756f6cec04bd533bdce05e160d6
pettik <65282144+pettik@users.noreply.github.com>
2023-08-09 15:33:39 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,11 @@
 const btnSend = document.getElementById('btn-send');
 const emailInput = document.getElementById('email');
 const emailError = document.querySelector('.email-error');
+const inputedEmail = document.querySelector('.inputedEmail');
+const firstSection = document.querySelector('#first');
+const secondSection = document.querySelector('#second');
+const secondSectionBtn = document.querySelector('#second .btn');
+
 
 btnSend.addEventListener('click', () => {
   const emailValue = emailInput.value.trim();
@@ -15,5 +20,19 @@ btnSend.addEventListener('click', () => {
     // Valid email format
     emailInput.classList.remove('error');
     emailError.classList.add('hidden');
+
+    //insert input email to second text
+    inputedEmail.textContent = emailValue;
+
+    //visible second section
+    firstSection.classList.add('d-none');
+    secondSection.classList.remove('d-none');
   }
 });
+
+
+secondSectionBtn.addEventListener('click',()=>{
+    emailInput.value = '';
+    firstSection.classList.remove('d-none');
+    secondSection.classList.add('d-none');
+})
