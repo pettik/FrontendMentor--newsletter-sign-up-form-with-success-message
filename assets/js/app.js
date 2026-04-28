ebf2ca91211db06f55c5bdf17a7701d05179690c
PetrB <petrbednarski@gmail.com>
2026-04-28 08:53:46 +0200

    html/js update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,64 @@
-'use strict';
-const btnSend = document.getElementById('btn-send');
-const emailInput = document.getElementById('email');
-const emailError = document.querySelector('.email-error');
-const inputedEmail = document.querySelector('.inputedEmail');
-const firstSection = document.querySelector('#first');
-const secondSection = document.querySelector('#second');
-const secondSectionBtn = document.querySelector('#second .btn');
+"use strict";
+const btnSend = document.getElementById("btn-send");
+const emailInput = document.getElementById("email");
+const emailError = document.querySelector(".email-error");
+const inputedEmail = document.querySelector(".inputedEmail");
+const firstSection = document.querySelector("#first");
+const secondSection = document.querySelector("#second");
+const secondSectionBtn = document.querySelector("#second .btn");
 
 // funkce pro ověření emailu
 const overeniEmailu = function () {
   const emailValue = emailInput.value.trim();
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (emailValue === '' || !emailPattern.test(emailValue)) {
+  if (emailValue === "" || !emailPattern.test(emailValue)) {
     // Invalid email format
-    emailInput.classList.add('error');
-    emailError.classList.remove('hidden');
+    emailInput.classList.add("error");
+    emailError.classList.remove("hidden");
   } else {
     // Valid email format
-    emailInput.classList.remove('error');
-    emailError.classList.add('hidden');
+    emailInput.classList.remove("error");
+    emailError.classList.add("hidden");
 
     //insert input email to second text
     inputedEmail.textContent = emailValue;
 
     //visible second section
-    firstSection.classList.add('d-none');
-    secondSection.classList.remove('d-none');
+    firstSection.classList.add("d-none");
+    secondSection.classList.remove("d-none");
   }
 };
 
-// kliknutí na btnSend v hlavním formuláři
-btnSend.addEventListener('click', () => {
+const form = document.getElementById("newsletter-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
   overeniEmailu();
 });
-
 // kliknutí na secondSectionBtn na vedlejším formuláři
-secondSectionBtn.addEventListener('click', () => {
-  emailInput.value = '';
-  firstSection.classList.remove('d-none');
-  secondSection.classList.add('d-none');
+secondSectionBtn.addEventListener("click", () => {
+  emailInput.value = "";
+  firstSection.classList.remove("d-none");
+  secondSection.classList.add("d-none");
 });
 
 // stisknutí klávesy ENTER
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter' || event.keyCode === 13) {
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" || event.keyCode === 13) {
     overeniEmailu();
   }
 });
 
 // změna obrázku na mobily
-const imageElement = document.getElementById('illustrationImage');
+const imageElement = document.getElementById("illustrationImage");
 
 function updateImageSrc() {
   if (window.innerWidth >= 768) {
     // Desktop screen (768px and above)
-    imageElement.src = 'assets/images/illustration-sign-up-desktop.svg';
+    imageElement.src = "assets/images/illustration-sign-up-desktop.svg";
   } else {
     // Phone screen (less than 768px)
-    imageElement.src = 'assets/images/illustration-sign-up-mobile.svg';
+    imageElement.src = "assets/images/illustration-sign-up-mobile.svg";
   }
 }
 
@@ -65,4 +66,4 @@ function updateImageSrc() {
 updateImageSrc();
 
 // Listen for window resize and update image src accordingly
-window.addEventListener('resize', updateImageSrc);
+window.addEventListener("resize", updateImageSrc);
