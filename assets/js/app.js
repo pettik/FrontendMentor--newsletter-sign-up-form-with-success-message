2d0c8cb723a4e5daffda7b3a819ebf6493a8549e
PetrB <petrbednarski@gmail.com>
2026-04-29 08:05:04 +0200

    css/js updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,69 +1,50 @@
 "use strict";
-const btnSend = document.getElementById("btn-send");
+
 const emailInput = document.getElementById("email");
 const emailError = document.querySelector(".email-error");
 const inputedEmail = document.querySelector(".inputedEmail");
 const firstSection = document.querySelector("#first");
 const secondSection = document.querySelector("#second");
 const secondSectionBtn = document.querySelector("#second .btn");
+const form = document.getElementById("newsletter-form");
+const imageElement = document.getElementById("illustrationImage");
 
-// funkce pro ověření emailu
 const overeniEmailu = function () {
   const emailValue = emailInput.value.trim();
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
   if (emailValue === "" || !emailPattern.test(emailValue)) {
-    // Invalid email format
     emailInput.classList.add("error");
     emailError.classList.remove("hidden");
-  } else {
-    // Valid email format
-    emailInput.classList.remove("error");
-    emailError.classList.add("hidden");
-
-    //insert input email to second text
-    inputedEmail.textContent = emailValue;
-
-    //visible second section
-    firstSection.classList.add("d-none");
-    secondSection.classList.remove("d-none");
+    return;
   }
-};
 
-const form = document.getElementById("newsletter-form");
+  emailInput.classList.remove("error");
+  emailError.classList.add("hidden");
+
+  inputedEmail.textContent = emailValue;
+
+  firstSection.classList.add("d-none");
+  secondSection.classList.remove("d-none");
+};
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   overeniEmailu();
 });
-// kliknutí na secondSectionBtn na vedlejším formuláři
+
 secondSectionBtn.addEventListener("click", () => {
   emailInput.value = "";
   firstSection.classList.remove("d-none");
   secondSection.classList.add("d-none");
 });
 
-// stisknutí klávesy ENTER
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Enter" || event.keyCode === 13) {
-    overeniEmailu();
-  }
-});
-
-// změna obrázku na mobily
-const imageElement = document.getElementById("illustrationImage");
-
 function updateImageSrc() {
-  if (window.innerWidth >= 768) {
-    // Desktop screen (768px and above)
-    imageElement.src = "assets/images/illustration-sign-up-desktop.svg";
-  } else {
-    // Phone screen (less than 768px)
-    imageElement.src = "assets/images/illustration-sign-up-mobile.svg";
-  }
+  imageElement.src =
+    window.innerWidth >= 768
+      ? "assets/images/illustration-sign-up-desktop.svg"
+      : "assets/images/illustration-sign-up-mobile.svg";
 }
 
-// Initial update based on the current screen width
 updateImageSrc();
-
-// Listen for window resize and update image src accordingly
 window.addEventListener("resize", updateImageSrc);
